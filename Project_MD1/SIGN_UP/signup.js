a18ada3950f0c5a0772f9afa6e0ef2deb85f3d82
nhdiemquynh <nguyenhoangdiemquynh@gmail.com>
2023-07-25 19:07:36 +0900

    2507 commit 2

--- Project_MD1/SIGN_UP/signup.js
+++ Project_MD1/SIGN_UP/signup.js
@@ -1,3 +1,19 @@
-$('.message a').click(function(){
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
- });
+let registerFormVisible = true;
+  // Lắng nghe sự kiện click trên phần tử có lớp "message"
+  const messageLinks = document.querySelectorAll('.message');
+  messageLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+      event.preventDefault(); // Ngăn chặn chuyển hướng đến đường dẫn href="#"
+      const registerForm = document.getElementById('register-form');
+      const loginForm = document.getElementById('login-form');
+      if (registerFormVisible) {
+        registerForm.style.display = 'none';
+        loginForm.style.display = 'block';
+      } else {
+        registerForm.style.display = 'block';
+        loginForm.style.display = 'none';
+      }
+      // Chuyển đổi trạng thái hiện tại của biểu mẫu
+      registerFormVisible = !registerFormVisible;
+    });
+  });
